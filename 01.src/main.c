a2cd3aa9f2e53a0058f72330ec53fbf1c5c2af05
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2024-05-15 00:20:48 +0200

    Added FlawFinder

--- 01.src/main.c
+++ 01.src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  printf("Hello, World!\n");
+  // Not allowing a char for \0. The linters should inform you...
+  char name[2] = "AA";
+  strncpy(name, "John", 2);
+  printf("Hello, %s!\n", name);
   return 0;
 }
