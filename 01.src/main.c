ab48fe3ffdbb5cca2550b58b71870ea562ea2dc2
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2024-05-16 23:27:58 +0200

    Manual resolution of linter smells

--- 01.src/main.c
+++ 01.src/main.c
@@ -3,8 +3,10 @@
 
 int main() {
   // Not allowing a char for \0. The linters should inform you...
-  char name[2] = "AA";
-  strncpy(name, "John", sizeof(name));
+  // @ts-ignore
+  char name[3] = "AA\0"; /* Flawfinder: ignore */
+  // strncpy is a safer way to copy strings
+  strncpy(name, "John", sizeof(name)-1); /* Flawfinder: ignore. Assured manually*/
   printf("Hello, %s!\n", name);
   return 0;
 }
